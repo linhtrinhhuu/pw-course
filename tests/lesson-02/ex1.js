814fb309cd1cd5f33f4c128d522900a706fd7fa3
linhtrinhhuu <hoolee2312@gmail.com>
2026-09-03 16:46:51 +0700

    feat: solve ex1 and ex2 of lesson 02

---
+++ tests/lesson-02/ex1.js
@@ -0,0 +1,8 @@
+// a. Khai báo một hằng số number với giá trị là 12.
+const number = 12;
+
+// b. Khai báo một biến name với giá trị là ”my number”
+let name = "my number";
+
+// c. Khai báo một biến isEven với giá trị là sai.
+let isEven = false;
