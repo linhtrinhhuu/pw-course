f83c7ba431f2d9ebf03cdc4f7cf7f83b1ce85ce7
linhtrinhhuu <hoolee2312@gmail.com>
2026-09-09 09:02:06 +0700

    feat: refactor code for ex5 of lesson 03

--- tests/lesson-03/ex5.js
+++ tests/lesson-03/ex5.js
@@ -1,7 +1,6 @@
 // In ra các giá trị chia hết cho 3 từ 1000 đến 2000
 for (let i = 1000; i <= 2000; i++) {
-    let kiemTraChiaHetChoBa = i % 3;
-    if (kiemTraChiaHetChoBa === 0) {
+    if (i % 3 === 0) {
         console.log(i);
     }
 }
