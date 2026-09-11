607f4ecd680bbd14a0cde04479c9a02798c3ad6d
linhtrinhhuu <hoolee2312@gmail.com>
2026-09-11 19:31:40 +0700

    chore: update output for ex3 of lesson 03

--- tests/lesson-03/ex3.js
+++ tests/lesson-03/ex3.js
@@ -5,7 +5,7 @@
 
 for (let i = 1; i <= 100; i++) {
     if (i % 2 === 0) {
-        console.log("Số " + i + " là số chắn");
+        console.log("Số " + i + " là số chẵn");
     }
     if (i % 2 !== 0) {
         console.log("Số " + i + " là số lẻ");
